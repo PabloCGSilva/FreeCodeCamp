bff8f4e0d488b9a70e326daef870b307f0daee71
PabloCGSilva <pablocarlos1982@gmail.com>
2022-01-16 17:50:05 -0300

    babel

--- myApp.js
+++ myApp.js
@@ -1,9 +1,8 @@
-import dotenv  from "dotenv"
-
+import dotenv from 'dotenv'
 dotenv.config()
 
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI);
+import mongoose from 'mongoose'
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
 let Person;
 
