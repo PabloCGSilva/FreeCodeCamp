185ec038d3bc42a74ac0903fd6011f227bb31609
PabloCGSilva <pablocarlos1982@gmail.com>
2022-01-16 17:30:22 -0300

    update

--- myApp.js
+++ myApp.js
@@ -2,6 +2,9 @@ import dotenv  from "dotenv"
 
 dotenv.config()
 
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI);
+
 let Person;
 
 const createAndSavePerson = (done) => {
