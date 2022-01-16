45abbbc5defb17d1539a28e39ec25fe648c76a96
PabloCGSilva <pablocarlos1982@gmail.com>
2022-01-16 12:49:27 -0300

    update

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ import 'dotenv/config.js'
 import express from 'express'
 import mongoose from 'mongoose'
 console.log(process.env)
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
 let Person;
 
